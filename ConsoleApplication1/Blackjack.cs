1c52f70d1f7c3f2ecfc17135403c2113d43d0fb7
filipepovoa <filipe.povoa@engenharia.ufjf.br>
2022-06-16 21:21:37 -0300

    first commit

---
+++ ConsoleApplication1/Blackjack.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ch10CardLib;
+
+namespace Ch10CardClient
+{
+    class Blackjack
+    {
+        public const int INTIAL_CASH = 500;
+        static void Main(string[] args)
+        {
+            Dealer d = new Dealer();
+            Player p = new Player(INTIAL_CASH, d);
+            char c;
+            int b;
+            Console.WriteLine("Your balance is: {0}", p.GetCash());
+            Console.WriteLine();
+            b = GetBet();
+            while (b != 0)
+            {
+                Console.Write("Your cards: ");
+                p.Hit();
+                p.Hit();
+                Console.WriteLine(p.HandToString());
+                d.Hit();
+                Console.Write("Dealer show card: ");
+                Console.WriteLine(d.HandToString());
+                Console.WriteLine("Your score: {0}", p.GetScore());
+                Console.Write("Would you like to (H)it or to (S)tay? ");
+                c = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                bool bust = false;
+                while (c == 'h' && !bust)
+                {
+                    p.Hit();
+                    Console.Write("Your cards: ");
+                    Console.WriteLine(p.HandToString());
+                    Console.WriteLine("Your score: {0}", p.GetScore());
+                    if (p.GetScore() > 21)
+                    {
+                        Console.WriteLine("You are BUST!!! Dealer WINS!");
+                        bust = true;
+                        break;
+                    }
+                    Console.Write("Would you like to (H)it or to (S)tay? ");
+                    c = Convert.ToChar(Console.ReadLine());
+                    Console.WriteLine();
+                }
+                if (bust)
+                    p.AddCash(-b);
+                else
+                {
+                    while (d.GetScore() < 17)
+                        d.Hit();
+                    Console.WriteLine("Dealer's cards: " + d.HandToString());
+                    Console.WriteLine("Dealer's score: {0}", d.GetScore());
+                    if (d.GetScore() > 21)
+                    {
+                        Console.WriteLine("The dealer is BUST!!!! You WIN!!! Congratulations!!!");    //YAY!
+                        p.AddCash(b);
+                    }
+                    else if (d.GetScore() < p.GetScore())
+                    {
+                        Console.WriteLine("You WIN!!! Congratulations!!!");  //YAY!
+                        p.AddCash(b);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Dealer WINS!");
+                        p.AddCash(-b);
+                    }
+                }
+                p.HandEmpty();
+                d.HandEmpty();
+                Console.WriteLine("Your balance is: {0}", p.GetCash());
+                Console.WriteLine();
+                b = GetBet();
+            }
+            if (p.GetCash() > 500)
+                Console.WriteLine("Congratulations and come back again soon.");
+            else if (p.GetCash() < 0)
+                Console.WriteLine("Billy, the Bone Breaker, is responsible for collections and will be in touch very soon.");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static int GetBet()
+        {
+            int b;
+            Console.Write("Bet amount in dollars: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            while (b > 250)
+            {
+                Console.WriteLine("You cannot bet more than $250! Bet again.");
+                Console.Write("Bet amount in dollars: ");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+            }
+            return b;
+        }
+    }
+}
---
+++ ConsoleApplication1/Class1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch10CardLib
+{
+    public enum Rank
+    {
+        Ace = 1,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+    }
+
+    public enum Suit
+    {
+        Club,
+        Diamond,
+        Heart,
+        Spade,
+    }
+
+    public class Card
+    {
+        public readonly Rank rank;
+        public readonly Suit suit;
+        private Card()
+        {
+            throw new System.NotImplementedException();
+        }
+        public Card(Suit newSuit, Rank newRank)
+        {
+            suit = newSuit;
+            rank = newRank;
+        }
+        public int GetRank()
+        {
+            return (int)rank;
+        }
+        public override string ToString()
+        {
+            string[] srank = { "X", "A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K" };
+            string[] ssuit = { "\x05", "\x04", "\x03", "\x06" };
+            return srank[(int)rank] + ssuit[(int)suit];
+        }
+    }
+
+    public class Deck
+    {
+        private Card[] cards;
+        public Deck()
+        {
+            cards = new Card[52];
+            for (int suitVal = 0; suitVal < 4; suitVal++)
+                for (int rankVal = 1; rankVal < 14; rankVal++)
+                    cards[suitVal * 13 + rankVal - 1] = new Card((Suit)suitVal, (Rank)rankVal);
+        }
+        public Card GetCard(int cardNum)
+        {
+            if (cardNum >= 0 && cardNum <= 51)
+                return (cards[cardNum]);
+            else
+                throw (new System.ArgumentOutOfRangeException("cardNum", cardNum, "Value must be between 0 and 51"));
+        }
+        public void Shuffle()
+        {
+            Card[] newDeck = new Card[52];
+            bool[] assigned = new bool[52];
+            Random sourceGen = new Random();
+            for (int i = 0; i < 52; i++)
+            {
+                int destCard = 0;
+                bool foundCard = false;
+                while (foundCard == false)
+                {
+                    destCard = sourceGen.Next(52);
+                    if (assigned[destCard] == false)
+                        foundCard = true;
+                }
+                assigned[destCard] = true;
+                newDeck[destCard] = cards[i];
+            }
+            newDeck.CopyTo(cards, 0);
+        }
+    }
+
+    public class Player
+    {
+        private int cash;
+        private Hand hand;
+        protected Dealer dealer;
+        public Player()
+        {
+            cash = 0;
+            hand = new Hand();
+            dealer = null;
+        }
+        public Player(int c, Dealer d)
+        {
+            cash = c;
+            hand = new Hand();
+            dealer = d;
+        }
+        public void Hit()
+        {
+            hand.AddCard(dealer.DealCard());
+        }
+        public int GetScore()
+        {
+            return hand.GetScore();
+        }
+        public void AddCash(int v)
+        {
+            cash += v;
+        }
+        public int GetCash()
+        {
+            return cash;
+        }
+        public string HandToString()
+        {
+            return hand.ToString();
+        }
+        public void HandEmpty()
+        {
+            hand.Empty();
+        }
+    }
+
+    public class Hand
+    {
+        private Card[] cards;
+        private int size;
+        public Hand()
+        {
+            cards = new Card[12];
+            size = 0;
+        }
+        public int GetScore()
+        {
+            int value = 0;
+            int countAce = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (cards[i].GetRank() <= 10 && cards[i].GetRank() >= 2)
+                    value += cards[i].GetRank();
+                else if (cards[i].GetRank() >= 11 && cards[i].GetRank() <= 13)
+                    value += 10;
+                else
+                {
+                    value += cards[i].GetRank();
+                    countAce++;
+                }
+            }
+            if (countAce >= 1 && value +10 <= 21)
+                value += 10;
+            return value;
+        }
+        public void AddCard(Card c)
+        {
+            cards[size++] = c;
+        }
+        public void Empty()
+        {
+            cards = new Card[12];
+            size = 0;
+        }
+        public override string ToString()
+        {
+            string hand = "";
+            for (int i = 0; i < size; i++)
+                hand += cards[i].ToString() + " ";
+            return hand;
+        }
+    }
+
+    public class Dealer : Player
+    {
+        Deck deck;
+        int count;
+        private void Init()
+        {
+            deck = new Deck();
+            deck.Shuffle();
+            count = 0;
+        }
+        public Dealer() : base()
+        {
+            Init();
+            dealer = this;
+        }
+        public Card DealCard()
+        {
+            if (count == 52)
+                Init();
+            Card c;
+            c = deck.GetCard(count++);
+            return c;
+        }
+    }
+}
